897ad482c03d0ee7d3d263a4b353bcab95515f27
TikhampornSky <tikhamporntan@gmail.com>
2023-04-20 18:02:43 +0700

    fix: room response

--- backend/src/room/room.service.ts
+++ backend/src/room/room.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -6,6 +6,14 @@ export class RoomService {
   constructor(private prismaService: PrismaService) {}
 
   async createRoom(userId: number) {
+    const room = await this.prismaService.room.findUnique({
+      where: {
+        patientId: userId,
+      },
+    });
+    if (room) {
+      throw new ForbiddenException('You have created a room already');
+    }
     try {
       const room = await this.prismaService.room.create({
         data: {
